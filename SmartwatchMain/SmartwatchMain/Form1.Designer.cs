e69f9a2679e3ce0f9abd616bd408fee1ba4062cf
Jimmy Habing <jimmy.habing@hotmail.com>
2018-04-05 16:13:31 +0200

    Added digital clock and buttons

--- SmartwatchMain/SmartwatchMain/Form1.Designer.cs
+++ SmartwatchMain/SmartwatchMain/Form1.Designer.cs
@@ -28,13 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harvest", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 133);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "buton 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "buton 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(573, 204);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Smartwatch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SmartwatchMain/SmartwatchMain/Form1.cs
+++ SmartwatchMain/SmartwatchMain/Form1.cs
@@ -12,9 +12,68 @@ namespace SmartwatchMain
 {
     public partial class Form1 : Form
     {
+        //Declaring fields
+        private int mode = 0;
+
+        //Initializing objects
+        Timer t = new Timer();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer interval
+            t.Interval = 1000; // 1000ms == 1s
+
+            t.Tick += new EventHandler(this.t_Tick);
+
+            t.Start();
+        }
+
+        //Timer event handler
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //get current time on initialization aka default system time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+
+            //time
+            string time = "";
+
+            //nul toevoegen wanneer getallen kleiner zijn dan 0
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+
+            if(mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+
+            //update text label
+            label1.Text = time;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
