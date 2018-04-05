de111a4dfda0dd22064dab02f3901eb877f436c0
Jimmy Habing <jimmy.habing@hotmail.com>
2018-04-06 00:15:55 +0200

    Completed the time function, it is now possible to edit your hours and minutes with buttonpresses

--- SmartwatchMain/SmartwatchMain/Form1.Designer.cs
+++ SmartwatchMain/SmartwatchMain/Form1.Designer.cs
@@ -34,11 +34,13 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -102,6 +104,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label2);
             this.tabPage2.Controls.Add(this.button4);
             this.tabPage2.Controls.Add(this.button3);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
@@ -112,13 +115,23 @@
             this.tabPage2.Text = "DisplayWeather";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(398, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Weatherdisplayer";
+            // 
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(249, 120);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 1;
-            this.button4.Text = "buton 4";
+            this.button4.Text = "buton 2";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -128,12 +141,13 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 0;
-            this.button3.Text = "buton 3";
+            this.button3.Text = "buton 1";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.label3);
             this.tabPage3.Controls.Add(this.button6);
             this.tabPage3.Controls.Add(this.button5);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
@@ -150,7 +164,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 1;
-            this.button6.Text = "buton 6";
+            this.button6.Text = "buton 2";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -159,9 +173,19 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 0;
-            this.button5.Text = "buton 5";
+            this.button5.Text = "buton 1";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(370, 55);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Weatherupdater";
             // 
             // Form1
             // 
@@ -172,6 +196,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Smartwatch";
@@ -180,7 +205,9 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -198,6 +225,8 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- SmartwatchMain/SmartwatchMain/Form1.cs
+++ SmartwatchMain/SmartwatchMain/Form1.cs
@@ -13,7 +13,11 @@ namespace SmartwatchMain
     public partial class Form1 : Form
     {
         //Declaring fields
-        //private int mode = 0;
+        int hh = DateTime.Now.Hour; //setting default system time
+        int mm = DateTime.Now.Minute; //Idem
+        int ss = DateTime.Now.Second; //Idem
+        int timemode = 0; //Mode 0 means default system time and mode 1 means a custom time
+        int timepress = 0; //0 means that only the hours will be edited and 1 means that the minutes will be edited
 
         //Initializing objects
         Timer mainclock = new Timer();
@@ -22,10 +26,11 @@ namespace SmartwatchMain
         {
             InitializeComponent();
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            //timer interval
-            mainclock.Interval = 1000; // 1000ms == 1s
+            //Timer interval for every 1 second (1000ms)
+            mainclock.Interval = 1000;
 
             mainclock.Tick += new EventHandler(this.t_Tick);
 
@@ -35,9 +40,27 @@ namespace SmartwatchMain
         //Timer event handler
         private void t_Tick(object sender, EventArgs e)
         {
-            //get current time on initialization aka default system time
-            int hh = DateTime.Now.Hour;
-            int mm = DateTime.Now.Minute;
+            if (timemode == 1) //if the time had changed to mode 1 (which means it is a custom time) it will still be updated every minute
+            {
+                ss = DateTime.Now.Second;
+                if (ss > 58)
+                {
+                    mm = mm + 1;
+                    if (mm > 58)
+                    {
+                        hh = hh + 1;
+                        if (hh > 22)
+                        {
+                            hh = 0;
+                        }
+                    }
+                }
+            }
+            else //if the time mode is on mode 0 then the clock will update every second from the system timer
+            {
+                hh = DateTime.Now.Hour; 
+                mm = DateTime.Now.Minute; 
+            }
 
             //time
             string time = "";
@@ -68,31 +91,73 @@ namespace SmartwatchMain
 
         private void button1_Click(object sender, EventArgs e)
         {
-            tabControl1.SelectedTab = tabPage2;
+            //Control structure to check in which time "edit" mode we are, 1 = incrementhours, 2 = increment minutes and with 0 we need to switch tabs
+                if (timepress == 1)
+                {
+                    //Set the clock 00 instead of 24 after it strikes 23
+                    if (hh > 22)
+                    {
+                        hh = 0; 
+                    }
+                    else
+                    {
+                        hh = hh + 1; //Increment the hours
+                    }
+                }
+                else if (timepress == 2)
+                {
+                    //Set the clock to 00 instead of 60 after it strikes 59
+                    if (mm > 58)
+                    {
+                        mm = 0; 
+                    }
+                    else
+                    {
+                        mm = mm + 1; //Increment the minutes
+                    }
+                }
+                else if (timepress == 0)
+                {
+                    //Tabswitcher
+                    tabControl1.SelectedTab = tabPage2;
+                }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //Needs the time update shit
+           //Setting the time mode to a custom time
+            timemode = 1;
+
+            //Set timepress when button 2 is clicked in the first tab (DisplayTime)
+            timepress = timepress + 1;
+            //Control structure to check when when the butten has been pressed 3 times. then it needs to be resetted
+            if (timepress >= 3)
+            {
+                timepress = 0;
+            }
         }
 
         private void switchTab1(object sender, EventArgs e)
         {
+            //Tabswitcher
             tabControl1.SelectedTab = tabPage1;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            //Tabswitcher
             tabControl1.SelectedTab = tabPage3;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            //Tabswitcher
             tabControl1.SelectedTab = tabPage2;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            //Tabswitcher
             tabControl1.SelectedTab = tabPage1;
         }
     }
