1cb83e49b64ea50f38ca1304e6ece5b356515427
Jimmy Habing <jimmy.habing@hotmail.com>
2018-04-06 02:03:45 +0200

    Updated

--- SmartwatchMain/SmartwatchMain/Form1.Designer.cs
+++ SmartwatchMain/SmartwatchMain/Form1.Designer.cs
@@ -90,10 +90,10 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(-5, -23);
+            this.tabControl1.Location = new System.Drawing.Point(-5, -4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(442, 196);
+            this.tabControl1.Size = new System.Drawing.Size(442, 177);
             this.tabControl1.TabIndex = 3;
             // 
             // tabPage1
@@ -187,7 +187,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(434, 170);
+            this.tabPage3.Size = new System.Drawing.Size(434, 151);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "UpdateWeather";
             this.tabPage3.UseVisualStyleBackColor = true;
